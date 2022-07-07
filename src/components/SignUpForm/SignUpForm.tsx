4c3e352c6a923fa15b64adb077407a83b1c04efd
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-07-07 22:01:39 +0200

    Implementation in form added

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,38 +1,62 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, { ChangeEvent, useState, useEffect } from "react";
+import { client, getProfiles } from "../../queries";
 
 const SignUpForm = () => {
-    const [inputNameValue, setInputNameValue] = useState<string>('');
-    const [inputImageValue, setInputImageValue] = useState<string>('');
+  const [inputNameValue, setInputNameValue] = useState<string>("");
+  const [inputImageValue, setInputImageValue] = useState<string>("");
 
-    const inputOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.name === 'username') {
-            setInputNameValue(e.target.value)
-        }
-        if (e.target.name === 'image') {
-            setInputImageValue(e.target.value);
-        }
+  const inputOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "username") {
+      setInputNameValue(e.target.value);
     }
-
-    const submitHandler = (e: any) => {
-        e.preventDefault();
-        console.log({inputNameValue, inputImageValue})
+    if (e.target.name === "image") {
+      setInputImageValue(e.target.value);
     }
+  };
 
-    return (
-        <div>
-            <form onSubmit={submitHandler}>
-                <label>Username:
-                    <input name="username" type="text" value={inputNameValue} onChange={inputOnChangeHandler}/>
-                </label>
-                <label>Image:
-                    <input name="image" type="text" value={inputImageValue} onChange={inputOnChangeHandler}/>
-                </label>
-                <button>submit</button>
-            </form>
-        </div>
-    );
-}
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    console.log({ inputNameValue, inputImageValue });
+  };
 
-export {
-    SignUpForm,
+  const fetchProfiles = async () => {
+    try {
+      const response = await client.query(getProfiles).toPromise();
+      console.log(response);
+    } catch (error) {
+      console.error({ error });
+    }
+  };
+
+  useEffect(() => {
+    fetchProfiles();
+  }, []);
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <label>
+          Username:
+          <input
+            name="username"
+            type="text"
+            value={inputNameValue}
+            onChange={inputOnChangeHandler}
+          />
+        </label>
+        <label>
+          Image:
+          <input
+            name="image"
+            type="text"
+            value={inputImageValue}
+            onChange={inputOnChangeHandler}
+          />
+        </label>
+        <button>submit</button>
+      </form>
+    </div>
+  );
 };
+
+export { SignUpForm };
